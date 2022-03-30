8b3a82bdd79c2049391203bcdaeac562a1c4c55d
sanni <opeyemisanni65@gmail.com>
2022-03-30 02:42:03 -0400

    tracked position of cursor to mouse with js

--- js/main.js
+++ js/main.js
@@ -6,4 +6,12 @@ function verifyPassword(){
     }
 }
 
-// ------cursor style-----------
+// // ------cursor style-----------
+// var cursor = document.getElementById("cursor");
+
+// document.onmousemove = function(e){     //this e gives the coordinates of x and y axis of the cursor div//
+
+//     cursor.style.left = (e.pageX - 25) + "px";       //position of x coordinate of cursor
+//     cursor.style.left = (e.pageY - 25) + "px";       //position of Y coordinates '' '' ''
+// //because the circle is 50px we minus 25 from both x and y coordinate position  to centre it on cursor
+// };
