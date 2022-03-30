71ef66bfb03f93a431166959e717b78544f8083a
sanni <opeyemisanni65@gmail.com>
2022-03-30 02:48:38 -0400

    made cursor only appear on mouse move and moved acript in html doc to main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,15 @@ function verifyPassword(){
     }
 }
 
-// // ------cursor style-----------
-// var cursor = document.getElementById("cursor");
+// ------cursor style-----------
+var cursor = document.getElementById("cursor");
+//everything inside onmousemove will occur as you move the mouse
+document.onmousemove = function(e){     //this e gives the coordinates of x and y axis of the cursor div//
 
-// document.onmousemove = function(e){     //this e gives the coordinates of x and y axis of the cursor div//
+    cursor.style.left = (e.pageX - 25) + "px";       //position of x coordinate of cursor
+    cursor.style.top = (e.pageY - 25) + "px";       //position of Y coordinates '' '' ''
+//because the circle is 50px we minus 25 from both x and y coordinate position  to centre it on cursor
+    cursor.style.display = "block";// cursor becomes visible
 
-//     cursor.style.left = (e.pageX - 25) + "px";       //position of x coordinate of cursor
-//     cursor.style.left = (e.pageY - 25) + "px";       //position of Y coordinates '' '' ''
-// //because the circle is 50px we minus 25 from both x and y coordinate position  to centre it on cursor
-// };
+
+};
